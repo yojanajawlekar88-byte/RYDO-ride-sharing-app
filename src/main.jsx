32ae8495732f327ffb9cf2b035ff09462c1be276
yojanajawlekar88-byte <yojanajawlekar88@gmail.com>
2026-08-24 21:42:57 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import { HashRouter } from "react-router-dom";
+
 import App from "./App";
 
-const basename = import.meta.env.BASE_URL.replace(/\/$/, "");
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
